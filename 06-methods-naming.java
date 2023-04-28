10fe210712462b7ad0349952f571f330a3105c3f
Stanislav <stanislav.vozikov@gmail.com>
2023-04-28 22:35:20 +0300

    added variable naming solution part 6

---
+++ 06-methods-naming.java
@@ -0,0 +1,108 @@
+// было
+
+Array<Message> listAll() {}
+
+// стало
+
+Array<Message> void getListAll() {}
+
+// было
+
+void newMeta () {}
+
+// стало
+
+void setMeta () {}
+
+// было
+
+void positive () {}
+
+// стало
+
+void isPositive () {}
+
+
+
+
+
+// было
+
+MessageInfo prepareInfo () {}
+
+// стало
+
+MessageInfo getInformationDataById () {}
+
+// было
+
+Integer getPositive () {}
+
+// стало
+
+Integer calculationPositiveRating () {}
+
+// было
+
+Boolean validation () {}
+
+// стало
+
+Boolean hasValidateError () {}
+
+
+
+
+
+
+// было
+
+void clear () {}
+
+// стало
+
+void resetForm () {}
+
+// было
+
+void setHiddenAxis () {}
+
+// стало
+
+void hiddeAxis () {}
+
+// было
+
+void exportTable () {}
+
+// стало
+
+void exportAllTickets () {}
+
+
+
+
+
+// было
+
+String formateDate () {}
+
+// стало
+
+String convertToUTCString () {}
+
+// было
+
+void changeProgram (Program program) {}
+
+// стало
+
+void setCurrentProgram (Program program) {}
+
+// было
+
+void moveTicket () {}
+
+// стало
+
+void setTicketStatusNotConfirmed () {}
